e12d19c467ec7a0038877ade8f401291f83d4063
edhojulio <edhojulio.ej@gmail.com>
2026-01-13 14:41:47 +0700

    feat: craete reusable module

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "master_public_ip" {
-  description = "Public IP address of the K8s master node."
-  value       = google_compute_instance.k8s_master.network_interface[0].access_config[0].nat_ip
-}
-
-output "worker_1_public_ip" {
-  description = "Public IP address of the K8s worker node 1."
-  value       = google_compute_instance.k8s_worker_1.network_interface[0].access_config[0].nat_ip
-}
